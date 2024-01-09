c15da7098540ebdf8b67d402f8c4b20b7f4ed912
flamintune <1853995253@qq.com>
2024-01-09 11:23:55 +0900

    style: 💄边框加上暗色模式

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,29 +1,24 @@
 document.body.style.backgroundColor = sessionStorage.getItem('bg');
 document.body.style.color = sessionStorage.getItem('cc');
+const frame_fill = document.getElementsByClassName('frame_fill')
+frame_fill.forEach(item => item.style.backgroundColor = sessionStorage.getItem('bg'))
 function theme() {
-     if ( sessionStorage.getItem('bg') === 'rgb(255, 255, 255)') {
-         
-            sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-            sessionStorage.setItem('cc', '#777');
-            
-         
-     }
+    if (sessionStorage.getItem('bg') === 'rgb(255, 255, 255)') {
+        sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
+        sessionStorage.setItem('cc', '#777');
+    }
     else if (sessionStorage.getItem('bg') == null || undefined) {
         sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
         sessionStorage.setItem('cc', '#777');
-        
     }
-    else if( sessionStorage.getItem('bg') === 'rgb(6, 23, 37)') {
-        
+    else if (sessionStorage.getItem('bg') === 'rgb(6, 23, 37)') {
         sessionStorage.setItem('bg', 'rgb(255, 255, 255)');
         sessionStorage.setItem('cc', '#333');
-        
-  
     }
 
-document.body.style.backgroundColor = sessionStorage.getItem('bg');
-document.body.style.color = sessionStorage.getItem('cc');
-
+    document.body.style.backgroundColor = sessionStorage.getItem('bg');
+    document.body.style.color = sessionStorage.getItem('cc');
+    frame_fill.forEach(item => item.style.backgroundColor = sessionStorage.getItem('bg'))
 }
 
 
